c28c0f57885e02624603a37259204869ed99eba4
c205 <axeman@bk.ru>
2018-02-19 15:08:03 +0300

    Домашнее задание к третьему уроку, в перврм задании так ине заработала замена

--- HW3/script.js
+++ HW3/script.js
@@ -2,15 +2,62 @@ window.onload = function() {
 
   // замена одинарных кавычек на двойные
 
-  function repWord () {
+  function repWord (str) {
 
   text = document.getElementById('txt');
 
-  text.replace(/'/g, '"');
+// не знаю почему не работает замена, PCRE: кавычка в начале строки или кавычка после пробела
+// или кавычка перед пробелом или кавычка в конце строки.
+
+  text.textContent.replace(/^'|(\s)'|'(\s)|'$/g, '$1"$2');
 
     return text;
 
   }
 
   repWord();
+
+  // проверка формы
+
+    function isName( value ) {
+        return (/[a-zA-Zа-яёА-Я]+/g).test( value );
+    }
+
+    function isPhone( value ) {
+        return (/^\+7\(\d{3}\)\d{3}-\d{4}$/g).test( value );
+    }
+
+    function isEmail( value ) {
+        return (/\w+\@mail\.ru|\w+\.\w+\@mail\.ru|\w+-\w+\@mail\.ru/g).test( value );
+    }
+
+    function isText( value ) {
+        return (/[\S]+/g).test( value );
+    };
+
+    document.getElementById('submit').addEventListener('click', function (evt) {
+        evt.preventDefault();
+
+        var user = document.getElementById('user_name');
+        var phone = document.getElementById('user_phone');
+        var email = document.getElementById('user_email');
+        var text = document.getElementById('user_text');
+
+        if ( !isName(user.value) ) {
+            user.classList.add('red');
+            document.getElementById("err_name").style.display="block";
+        }
+        if ( !isPhone(phone.value) ) {
+            phone.classList.add('red');
+            document.getElementById("err_phone").style.display="block";
+        }
+        if ( !isEmail(email.value) ) {
+            email.classList.add('red');
+            document.getElementById("err_email").style.display="block";
+        }
+        if ( !isText(text.value) ) {
+            text.classList.add('red');
+            document.getElementById("err_text").style.display="block";
+        }
+    });
 }; // end window.onload
