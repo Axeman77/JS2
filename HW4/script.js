f2bb8738cb27d05ac90b012f78b66241c066e688
c205 <axeman@bk.ru>
2018-02-20 15:54:46 +0300

    Домашнее задание 4 попытка сменить класс

--- HW4/script.js
+++ HW4/script.js
@@ -1,7 +1,9 @@
 (function ($) {
     $(function () {
        $('.tabs__caption').on('click', 'li', function (event) {
-
+           $('ul.tabs__caption').on('click', 'li', function() {
+               $(this).toggleClass('active');
+           })
        });
     });
 })(jQuery);
