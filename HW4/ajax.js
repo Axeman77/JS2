dd727590e80d8373f1786639f044488bf3a15a47
c205 <axeman@bk.ru>
2018-02-21 15:49:22 +0300

    Домашнее задание 4. Продолжаем.

--- HW4/ajax.js
+++ HW4/ajax.js
@@ -1,13 +1,8 @@
 (function ($) { // Ждём загрузки страницы
     $('#submit').on('click',  function (event) {
-        jQuery.ajax({    // Начало конструкции для работы с Ajax через jQuery
-            type: 'POST', // Метод, которым получаем данные из формы
-            url: 'http://api.spacenear.ru/index.php', // Обработчик формы.
-            data: jQuery("#form").serialize(), // Этот метод, берет форму с id=form и достает оттуда данные
-            success: function(html) {    // функция выполняемая при успешном отправлении данных
-            jQuery("body").empty();    // очищаем тело документа
-            jQuery("body").append("<h2><center>Ваше сообщение успешно отправлено.</center></h2>"); // вставляем сообщение об успехе
-            }
+        jQuery.ajax({
+
+        }
         });
     });
 })(jQuery);
