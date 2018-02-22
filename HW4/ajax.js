cbd60148eed9be707354e670189af2c4ab96e7fb
Axeman77 <axeman@bk.ru>
2018-02-22 13:22:50 +0300

    По клику мышки выбирает несколько городов без пробела

--- HW4/ajax.js
+++ HW4/ajax.js
@@ -1,9 +1,39 @@
-(function ($) { // Ждём загрузки страницы
-    $('#submit').on('click',  function (event) {
-        jQuery.ajax({
+(function ($) {
+  $(function () {  // Ждём загрузки страницы
+    $('#city').on('keyup',  function (event) { // срабатывет на отпускание кнопки
+      $('#city-list').empty();
+          if($(this).val().length > 2) { // нужно минимум 3 буквы
+            var value = $(this).val(); // здесь начало названия города
 
-        }
+            $.post('http://api.spacenear.ru/index.php', {pattern: value}, function(data) {
+               try {
+                    var result = JSON.parse(data);
+
+                    if (result.length > 0) {
+                      $('#city-list').addClass('active');
+                      for (var i = 0; i < result.length; i++) {
+                        var city = $('<div>').addClass('list').append(result[i]['name']);
+                        $('#city-list').append(city);
+                      }
+                    }
+                  } catch(err) {
+                    console.log(err);
+                  }
+                });
+          }
         });
-    });
-})(jQuery);
 
+        // выбор города из списка городов по клику мыши
+
+        $('#form').on('click', '#city-list', function(event) {
+          var cityName = $(this).text();
+
+
+          if (cityName.length > 0) {
+              $('#city').val(cityName);
+            }
+
+            $('#city-list').empty().removeClass('active');
+        });
+    }); // end ready
+})(jQuery);
